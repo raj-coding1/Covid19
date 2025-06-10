4dc813b403318de8b8e5bd85ad5fbc059cef3b47
raj-coding1 <152235215+raj-coding1@users.noreply.github.com>
2025-06-10 23:28:55 +0530

    Update main.py

--- Covid_19/main.py
+++ Covid_19/main.py
@@ -18,7 +18,7 @@ st.set_page_config(
 
 df_recover = pd.read_csv('https://raw.githubusercontent.com/raj-coding1/Covid19/refs/heads/main/Datasets/covid_19_recovered_v1_lyst1747728719904.csv')
 df_confirm = pd.read_csv('https://raw.githubusercontent.com/raj-coding1/Covid19/refs/heads/main/Datasets/covid_19_confirmed_v1_lyst1747728690432.csv')
-df_death = pd.read_csv(r'C:\Users\Personal PC\Downloads\covid_19_deaths_v1_lyst1747728711771.csv')
+df_death = pd.read_csv('https://raw.githubusercontent.com/raj-coding1/Covid19/refs/heads/main/Datasets/covid_19_deaths_v1_lyst1747728711771.csv')
 #initial preprocess
 df_recover.columns = df_recover.iloc[0]
 df_recover = df_recover[1:].reset_index()
